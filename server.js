5a151c580d323b599e9cb8aa227dcba7cc0fbf28
Luanafollegati <luana.guerreiro@aluno.senai.br>
2025-08-25 09:57:51 -0300

    feat: edita arquivo

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 import express from "express";
-import bruxos from "./src/data/bruxos.js";
+import dados from "./src/data/dados.js"
+
+const { bruxos, casas, varinhas, animais, pocoes } = dados;
 
 const app = express();
 app.use(express.json());
@@ -18,7 +20,8 @@ app.get ("/bruxos/:id", (req, res) => {
     const id = parseInt(req.params.id);
 
     const bruxo = bruxos.find(b => b.id === id);
-
+    
+    
 
 if(bruxo) {
     res.status(200).json(bruxo);
@@ -27,10 +30,6 @@ if(bruxo) {
         mensagem:"Bruxo não encontrado!"
     })
   }
-});
-
-app.listen(serverPort, () => {
-    console.log("Servidor esta rodando...");
 });
 
 app.get("/bruxos/nome/:nome", (req, res) => {
@@ -46,5 +45,90 @@ app.get("/bruxos/nome/:nome", (req, res) => {
             mensagen:"Bruxo não encontrado!"
         })
     }
-})
+});
+
+
+app.get ("/bruxos",(req, res) => {
+    let bruxos = req.params.nome;
+    bruxos = bruxos.toLowerCase();
+
+    const bruxosFiltrados = bruxos.filter(b => b.bruxos.toLowerCase().includes(bruxos));
+
+
+    if(bruxos.lenght > 0){
+        res.status(200).json(bruxos);
+    } else {
+        res.status(404).json({
+            mensagem:"Nenhum bruxo encontrado!"
+        })
+    }
+});
+
+app.get("/casas", (req, res) =>{
+    let casas = req.params.nome;
+    casas = casas.toLowerCase();
+    const casasFiltrados = casas.filter(c => c.casas.toLowerCase().includes(casas));
+
+
+    if(casas.lenght > 0){
+        res.status(200).json(casas);
+    } else {
+        res.status(404).json({
+            mensagem:"Nenhuma casa encontrado!"
+        })
+    }
+});
+
+app.get("/varinhas", (req, res) =>{
+    let varinhas = req.params.nome;
+    varinhas = varinhas.toLowerCase();
+
+    const varinhasFiltrados = varinhas.filter(v => v.varinhas.toLowerCase().includes(varinhas));
+
+
+    if(varinhas.lenght > 0){
+        res.status(200).json(varinhas);
+    } else {
+        res.status(404).json({
+            mensagem:"Nenhuma varinha encontrado!"
+        })
+    }
+});
+
+app.get("/animais", (req, res) =>{
+    let animais = req.params.nome;
+    animais = animais.toLowerCase();
+
+    const animaisFiltrados = animais.filter(a => a.animais.toLowerCase().includes(animais));
+
+
+    if(animais.lenght > 0){
+        res.status(200).json(animais);
+    } else {
+        res.status(404).json({
+            mensagem:"Nenhum animal encontrado!"
+        })
+    }
+});
+
+app.get("/pocoes", (req, res) =>{
+    let pocoes = req.params.nome;
+    pocoes = pocoes.toLowerCase();
+
+    const pocoesFiltrados = pocoes.filter(p => p.pocoes.toLowerCase().includes(pocoes));
+
+
+    if(pocoes.lenght > 0){
+        res.status(200).json(pocoes);
+    } else {
+        res.status(404).json({
+            mensagem:"Nenhuma poção encontrado!"
+        })
+    }
+});
+
+app.listen(serverPort, () => {
+    console.log("Servidor esta rodando...");
+});
+
 
